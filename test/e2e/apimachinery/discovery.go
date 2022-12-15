abcb3d0c8338a4f9a75c562952547330a3721c2e
Lukasz Szaszkiewicz <lukasz.szaszkiewicz@gmail.com>
2022-12-15 13:40:34 +0100

    UPSTREAM: <carry>: test/e2e:switch to logicalcluster.v3

--- test/e2e/apimachinery/discovery.go
+++ test/e2e/apimachinery/discovery.go
@@ -20,7 +20,7 @@ import (
 	"context"
 	"strings"
 
-	"github.com/kcp-dev/logicalcluster/v2"
+	"github.com/kcp-dev/logicalcluster/v3"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	utilversion "k8s.io/apimachinery/pkg/util/version"
